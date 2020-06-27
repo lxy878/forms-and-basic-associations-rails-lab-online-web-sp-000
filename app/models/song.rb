54d7791103ab7f33352e423e6c38e9e3418520fd
Xiaoyi Liu <lxy878@gmail.com>
2020-06-27 09:24:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,11 @@ class Song < ActiveRecord::Base
   # add associations here
   belongs_to :artist
   belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist : nil
 end
