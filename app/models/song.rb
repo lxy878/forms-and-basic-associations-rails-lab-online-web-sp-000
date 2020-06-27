cecb932021e4c724ee7ac7442fe0da05f9966c3b
Xiaoyi Liu <lxy878@gmail.com>
2020-06-27 11:51:27 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,23 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each {|content| self.notes.build(content: content) if !content.empty?}
+  end
+
+  def note_contents
+    self.notes.collect {|note| note.content}
+  end
+
 end
