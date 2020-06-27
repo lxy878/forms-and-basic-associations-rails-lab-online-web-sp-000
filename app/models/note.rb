06cb98eafb43c35ad78e8b8a777abcb23b8d3498
Xiaoyi Liu <lxy878@gmail.com>
2020-06-27 09:24:04 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,12 +1,5 @@
 class Note < ActiveRecord::Base
   # add associations here
   belongs_to :song
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
-
-  def artist_name
-    self.artist ? self.artist : nil
   end
 end
